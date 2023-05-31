48f06e96ae50fcc07aea5bc21e0e60291b7d9e53
aush <127614517+aushass@users.noreply.github.com>
2023-05-31 18:19:23 +0300

    вывод информации о чае и гостях на мероприятии

--- TeaTime/page/wEventSee.xaml.cs
+++ TeaTime/page/wEventSee.xaml.cs
@@ -44,7 +44,46 @@ namespace teaTime
         }
         private void loadData()
         {
-            
+            using (KotkovaISazonovaEntities_ DB = new KotkovaISazonovaEntities_())
+            {
+                teaProgramm.ItemsSource = ConvertTea(DB.PertyTea.ToList());
+                guestsList.ItemsSource = ConvertGuest(DB.goPhoto.ToList());
+            }
+        }
+        public List<DataSee> ConvertGuest(List<goPhoto> List)
+        {
+            List<DataSee> val = new List<DataSee>();
+            int j = 1;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (DateTime.Parse(aDate.Text) == List[i].Date)
+                {
+                    val.Add(new DataSee
+                    {
+                        num = j++,
+                        value = List[i].surname + " " + List[i].name + " " + List[i].middleName
+                    });
+                }
+
+            }
+            return val;
+        }
+        public List<DataSee> ConvertTea(List<PertyTea> List)
+        {
+            List<DataSee> val = new List<DataSee>();
+            int j = 1;
+            for (int i = 0; i < List.Count; i++)
+            {
+                if (DateTime.Parse(aDate.Text) == List[i].Date)
+                {
+                    val.Add(new DataSee
+                    {
+                        num = j++,
+                        value = List[i].Name.ToString()
+                    });
+                }
+            }
+            return val;
         }
         private void bClose_Click(object sender, RoutedEventArgs e)
         {
